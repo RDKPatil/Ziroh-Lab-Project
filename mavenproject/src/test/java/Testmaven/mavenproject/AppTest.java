6ec5ec8d283ad3625ebc665705f7dc397b9c8112
Pushpa-Patil <86980721+Pushpa-Patil@users.noreply.github.com>
2021-10-03 14:02:10 +0530

    Update AppTest.java

--- mavenproject/src/test/java/Testmaven/mavenproject/AppTest.java
+++ mavenproject/src/test/java/Testmaven/mavenproject/AppTest.java
@@ -3,6 +3,9 @@ package Testmaven.mavenproject;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
@@ -10,6 +13,7 @@ import org.junit.Test;
 
 import com.ZirohLabMethods.ResultS;
 import com.ZirohLabMethods.ZirohLabsMethods;
+
 
 import junit.framework.Assert;
 
@@ -61,4 +65,68 @@ public class AppTest
      Assert.assertEquals(GetConnectionResult.getErrCode(), 0);
 	 System.out.println("Connected......!!!!!!");
 }
+	 @Test
+	    public void UploadFileTest()
+	    {
+		    ZirohLabsMethods app = new ZirohLabsMethods();
+	        
+	        String localPath = "/UploadFile";
+	        String fileName = "UploadFile"+Math.abs(new Random().nextInt());
+
+	        try {
+	            PrintWriter writer = new PrintWriter(localPath + fileName);
+	            writer.write("This is the test file for the UploadFile() function.");
+	            writer.close();
+	            File localFile = new File(localPath + fileName);
+
+	       	    ResultS UploadResult = new ResultS();
+	            FutureTask<ResultS> GetconnectionFuturetask = app.GetconnectionFuturetask();
+	            UploadResult = getTaskResult(GetconnectionFuturetask);
+
+	   
+	            Assert.assertEquals(UploadResult.getErrCode(),0);
+	            System.out.println("UploadFile is working fine");
+	            
+
+	          
+	        }
+	        catch(Exception e)
+	        {
+	            System.out.println(e);
+	        }
+	    }
+	 
+	 
+
+	    @Test
+	    public void DownloadFileTest()
+	    {
+	       
+	    	 ZirohLabsMethods app = new ZirohLabsMethods();
+	        String localPath = "./DownloadFile/";
+	       
+	        String fileName = "UploadFile"+Math.abs(new Random().nextInt());
+	        
+
+	        
+	        try {
+	            PrintWriter writer = new PrintWriter(localPath + fileName);
+	            writer.write("This is the test file for the DownloadFile() function.");
+	            writer.close();
+	            File localFile = new File(localPath + fileName);
+	            ResultS DownloadResult = new ResultS();
+	            FutureTask<ResultS> downloadfuturetask = app.DownloadFileFututreTask(fileName);
+	            DownloadResult = getTaskResult(downloadfuturetask);
+	            
+	            Assert.assertEquals(DownloadResult.getErrCode(),0);
+	            System.out.println("DownloadFile is working fine");
+	            
+	        }
+	        catch(Exception e)
+	        {
+	            System.out.println(e);
+	        }
+	    }
+
+	 
 }
